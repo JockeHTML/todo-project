4d20c16968e2a89a6717da5ed19baacaa7aaa05c
Joakim Andersson <joakim.aandersson@live.se>
2020-12-08 14:42:13 +0100

    added procfile and updated ports

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //jshint esversion:6
 
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -12,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-joakim:Test-123@cluster0.rqgdg.mongodb.net/todolistDB", {useNewUrlParser: true}, { useUnifiedTopology: true });
 
 const itemsSchema = {
   name: String
@@ -122,6 +123,11 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(process.env.PORT, function() {
+  console.log("Server started");
 });
